dbf9e821abd155bb275058aedd066e07e5d29f9f
4upak <serg.chupak@gmail.com>
2022-11-27 21:51:44 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass'],
+
+    build: {
+        transpile: ['vuetify','axios'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+    },
+
+})
