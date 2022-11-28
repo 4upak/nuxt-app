ae2a43a77dc1ca9dd393cf9611320c3e112fef79
4upak <serg.chupak@gmail.com>
2022-11-28 21:49:25 +0200

    pinia problem solved

---
+++ pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+test template
+  {{name}} - {{isMobile}}
+</template>
+
+<script>
+import { mapState, mapActions} from 'pinia'
+import { useStore } from '../stores/MainStore'
+
+export default {
+  computed: {
+    ...mapState(useStore, ['count','name', 'isMobile','items','hasChanged'])
+
+  },
+  methods: {
+    ...mapActions(useStore, ['getMobileCheck'])
+  },
+  mounted() {
+    this.getMobileCheck()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
